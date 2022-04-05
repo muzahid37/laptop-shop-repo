ac36381903f6d2380e50268b3577863e9af6256d
muzahid37 <muzahidul372709@gmail.com>
2022-04-05 15:10:32 +0600

    add click hare on 404 page

--- src/Components/NothingPage/NothingPage.js
+++ src/Components/NothingPage/NothingPage.js
@@ -6,6 +6,7 @@ const NothingPage = () => {
         <div>
            <h2>404 page</h2>
            <p>SORRY!! NOTHIN FOUND</p>
+           <p>If you want to go home page <a href="/home">click hare</a></p>
         </div>
     );
 };
